acfac90c3da1f16f05e22b9dac1340b870c3b1fe
Maxi <max_jose87@hotmail.com>
2020-08-19 01:48:20 -0300

    initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+// import { Container, Col, Row } from "react-bootstrap";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <Fragment key="fragment-layout">
+      <Header />
+      {children}
+      {/* <Container fluid>
+        <Row>
+          <Col xs={12}>{children}</Col> */}
+      {/* </Row>
+      </Container> */}
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+import Layout from "../src/components/Layout";
+import AppContext from "../src/context";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <AppContext>
+        <App />
+      </AppContext>
+    </Layout>
   </React.StrictMode>,
   rootElement
 );
